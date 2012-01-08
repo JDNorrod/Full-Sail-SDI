1059761d69500a5032331fcffb45a1f12482c726
Jonathan <muse@Jonathans-MacBook-Pro.local>
2012-01-08 14:55:36 -0700

    Starting layout

--- sdi-project2/Norrod_Jay_assignment2.js
+++ sdi-project2/Norrod_Jay_assignment2.js
@@ -1,1 +1,14 @@
-alert("JavaScript works!");
+//  SDI Project 2 Functions and What not
+//  Jay Norrod
+//  SDI January 2012
+
+//********************Variables
+
+
+
+//********************methods (or functions whatever you want to call them)
+
+
+
+//******************** "The Code"
+
