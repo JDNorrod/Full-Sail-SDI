8807b3b31f9d0acd3fefecda4f73df6f5191a261
Jonathan <muse@Jonathans-MacBook-Pro.local>
2012-01-08 15:16:13 -0700

    Function declarations added

--- sdi-project2/Norrod_Jay_assignment2.js
+++ sdi-project2/Norrod_Jay_assignment2.js
@@ -3,11 +3,21 @@
 //  SDI January 2012
 
 //********************Variables
-
+var a_pilots[];
+var a_helicopter[];
+var discExists = false;
+var discPriority = 0;
+var a_discrepancies["landing light", "bird strike", "Comm 2"];
+var canFlyNights = true;
+var isGrounded = false;
 
 
 //********************methods (or functions whatever you want to call them)
-
+var hasLanded = function (helicopter); 				// Procedure
+var isBroken = function (pilot, discrepancy); 		// Boolean Function
+var checkPriority = function (priority); 			// Number function
+var orderPart = function (priority, part); 			// string function
+var notifyPilots = function (priority, a_pilots[]); // array function
 
 
 //******************** "The Code"
