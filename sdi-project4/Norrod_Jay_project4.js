bd1c9a8d3765e529c3b5ae840a9974038a042017
Jonathan <Kingjadien@aol.com>
2012-01-21 22:36:20 -0700

    email valid implemented

--- sdi-project4/Norrod_Jay_project4.js
+++ sdi-project4/Norrod_Jay_project4.js
@@ -1,1 +1,23 @@
-alert("JavaScript works!");
+//****************************variables
+var myEmail = "jonathan.@aol.com";
+
+
+//****************************functions
+var checkEmail = function (email){  //validates the format of an email address
+	var reEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	return reEmail.test(email); // return true if format matches
+}
+
+
+
+
+
+//****************************Proof that I'm a programming ninja
+
+if (checkEmail(myEmail)){
+	console.log("email is valid");
+}
+else{
+	console.log("HACKER ALERT!  You suck as a hacker, you entered the wrong format");
+}
+
